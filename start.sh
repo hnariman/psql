ea6f5097739a89c80c40604b6f37affc62008314
Nariman Huseynov <hnariman@gmail.com>
2022-09-11 18:12:06 +0400

    updated psql commands samples

--- start.sh
+++ start.sh
@@ -9,10 +9,19 @@ PSQL="docker exec -it test-psql psql -U postgres"
 echo "TESTING IF WE HAVE ANY TALBES"
 $PSQL -c "\dt"
 
-$PSQL -c "create table testing_table (\
+$PSQL -c "create table if not exists testing_table (\
     id serial, \
-    name varchar(50) not null \
+    first_name varchar(50) not null \
   )"
+
+echo "rename name field to first_name in table testing_table"
+$PSQL -c "alter table testing_table rename column name to first_name"
+
+echo "adding lastname field to testing_table"
+$PSQL -c "alter table testing_table add column if not exists last_name varchar(50) not null "
+
+echo "adding age field to testing_table"
+$PSQL -c "alter table testing_table add column if not exists age int not null "
 
 echo "check if we have some new tables"
 
@@ -22,6 +31,11 @@ $PSQL -c "\dt"
 echo "describe table testing_table"
 $PSQL -c  "\d testing_table"
 
-
 echo "inserting sample values"
 $PSQL -c"table testing_table"
+
+echo "show databases"
+$PSQL -c "\l"
+
+echo "select all from "
+$PSQL -c "select * from testing_table"
